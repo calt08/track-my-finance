0910751fb1eb9a0d7f864c0dabde4cc16fd3cebe
aledeltoro <alejandroencarnacion1@gmail.com>
2021-04-11 16:03:32 -0400

    Implement mongoDB config using mongoose

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config()
+
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.8pti2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+})
+
+const db = mongoose.connection;
+
+db.on("open", () => {
+	console.log("we're connected!");
+})
