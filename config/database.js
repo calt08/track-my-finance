d3f064430331d2233dd6cd79e0429bf2d121692a
Alejandro Encarnacion <aencarnacion@truora.com>
2021-04-15 02:11:54 -0400

    Add route to create users

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
-const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.8pti2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.8pti2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 
 db.on("open", () => {
-	console.log("we're connected!");
-})
+  console.log("we're connected!");
+});
