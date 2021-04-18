b461bd51bb142e24da5fe018235a629636e465de
calt08 <cesarlopez2808@gmail.com>
2021-04-18 10:39:12 -0400

    Add simple authentication

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { genSalt, hash, compare } = require("bcryptjs");
 
 const createUser = async (req, res) => {
   const user = new User(req.body);
@@ -43,8 +45,24 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  //Checking if email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return res.status(400).send("Please provide a valid Email and password.");
+  //Checking password
+  const validPass = await compare(req.body.password, user.password);
+  if (!validPass)
+    return res.status(400).send("Please provide a valid Email and password.");
+
+  //Assign Token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.status(200).header("Authorization", token).send(token);
+};
+
 module.exports = {
   createUser,
   deleteUser,
   fetchUserByID,
+  login,
 };
