be320781408568d892b444d13a34f8ddead03fe7
Alejandro Encarnacion <aencarnacion@truora.com>
2021-04-18 14:17:14 -0400

    Create route to request user update by ID

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createUser, deleteUser, fetchUserByID } = require("../controllers/user");
+const { createUser, deleteUser, fetchUserByID, updateUserByID } = require("../controllers/user");
 
 router.post("/users", createUser);
 router.get("/users/:id", fetchUserByID);
 router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUserByID);
 
 module.exports = router;
