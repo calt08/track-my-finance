a6f3ed48916b47f44cdf1aed2d2dba4f2f09d837
aledeltoro <alejandroencarnacion1@gmail.com>
2021-04-19 23:09:10 -0400

    Implement controllers to fetch all accounts or by ID

--- controllers/account.js
+++ controllers/account.js
@@ -1,4 +1,5 @@
 const Account = require("../models/account");
+const User = require("../models/user");
 
 const createAccount = async (req, res) => {
   const account = new Account({ ...req.body, userID: res.locals.user });
@@ -10,6 +11,40 @@ const createAccount = async (req, res) => {
   }
 };
 
+const fetchAccounts = async (req, res) => {
+	const userID = res.locals.user;
+
+	try {
+		const accounts = await Account.find({userID});
+
+		if (!accounts) {
+			return res.status(400).send({ status: 400, message: "accounts not found"});
+		}
+
+		res.status(200).send(accounts);
+	} catch (err) {
+		res.status(500).send();
+	}
+}
+
+const fetchAccountByID = async (req, res) => {
+	const id = req.params.id;
+
+	try {
+		const account = await Account.findById(id);
+
+		if (!account) {
+			return res.status(400).send({ status: 400, message: "account not found"});
+		}
+
+		res.status(200).send(account);
+	} catch (err) {
+		res.status(500).send();
+	}
+}
+
 module.exports = {
   createAccount,
+	fetchAccounts,
+	fetchAccountByID,
 };
