f09e8197d2873f4f174001ffacb9d98b2b40a40b
kelvinlee0225 <kelvinlee225@hotmail.com>
2021-04-20 17:55:59 -0400

    created routes to create transaction

---
+++ routes/transaction.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    createTransaction
+} = require("../controllers/transaction")
+
+
+
+router.post("/accounts/:account_id/transactions");
+
+module.exports = router;
