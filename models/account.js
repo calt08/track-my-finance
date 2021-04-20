cda7a66fe6671eda3cd22fdf86efdc1728733c17
calt08 <cesarlopez2808@gmail.com>
2021-04-19 22:07:13 -0400

    Add CASCADE delete to accounts

--- models/account.js
+++ models/account.js
@@ -1,29 +1,43 @@
 const mongoose = require("mongoose");
+const transactionSchema = require("./transaction");
 
 const accountTypes = ["General", "Cash", "Credit card", "Savings"];
 
 const accountSchema = new mongoose.Schema({
   name: {
     type: String,
-		required: true,
+    required: true,
   },
   type: {
     type: String,
     enum: accountTypes,
-		default: accountTypes[0],
+    default: accountTypes[0],
   },
   currency: {
     type: String,
   },
   amount: {
     type: mongoose.Schema.Types.Decimal128,
-		required: true,
-		default: 0,
+    required: true,
+    default: 0,
   },
   userID: {
     type: mongoose.Schema.Types.ObjectId,
-		required: true,
+    required: true,
   },
+});
+
+accountSchema.pre("findOneAndDelete", async function (next) {
+  const query = this;
+  const id = query._conditions._id;
+
+  try {
+    await transactionSchema.deleteMany({ userID: id });
+  } catch (err) {
+    throw new Error("Failed deleting account dependencies.");
+  }
+
+  next();
 });
 
 const Account = mongoose.model("account", accountSchema);
