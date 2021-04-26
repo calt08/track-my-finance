34c00558801a4d5a1a2e1056b411c159c88144d2
aledeltoro <alejandroencarnacion1@gmail.com>
2021-04-26 03:36:18 -0400

    Add documentation endpoint through swagger

--- routes/router.js
+++ routes/router.js
@@ -6,11 +6,13 @@ const router = express.Router();
 const userRoute = require("./user");
 const accountRoute = require("./account");
 const transactionRoute = require("./transaction");
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 // Routes middlewares
 router.use("/users", userRoute);
 router.use("/accounts", accountRoute);
-router.use("/transactions", transactionRoute)
+router.use("/transactions", transactionRoute);
+router.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 module.exports = router;
